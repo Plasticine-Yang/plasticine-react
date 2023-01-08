c4cdeafdd99bd8ac4ae5f8c068bc682448c08529
plasticine9750 <975036719@qq.com>
2023-01-08 15:44:48 +0800

    chore(commitlint): add scopes prompt

--- .commitlintrc.cjs
+++ .commitlintrc.cjs
@@ -1,6 +1,13 @@
+const { readdirSync } = require('fs')
+const { resolve } = require('path')
+const packages = readdirSync(resolve(__dirname, 'packages'))
+
 /**
  * @type { import('cz-git').UserConfig }
  */
 module.exports = {
   extends: ['@commitlint/config-conventional'],
+  prompt: {
+    scopes: [...packages],
+  },
 }
