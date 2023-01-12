9dcd424742185a7df56d2c252841473a089d7ef7
plasticine9750 <975036719@qq.com>
2023-01-12 18:23:36 +0800

    feat(react-reconciler): extract platform-related functions

--- packages/react-reconciler/src/host-config.ts
+++ packages/react-reconciler/src/host-config.ts
@@ -3,4 +3,29 @@
  */
 
 /** @description createRoot(container) 中 container 的类型 */
-export type Container = unknown
+type Container = unknown
+
+/**
+ * @description 创建具体平台实例 -- 比如创建 DOM 元素
+ */
+function createInstance(...args: any[]) {
+  return {} as any
+}
+
+/**
+ * @description 创建具体平台的文本实例 -- 比如创建 DOM 中的 Text
+ */
+function createTextInstance(...args: any[]) {
+  return {} as any
+}
+
+/**
+ * @description 往 parent 中插入元素
+ */
+function appendInitialChild(...args: any[]) {
+  return {} as any
+}
+
+export type { Container }
+
+export { createInstance, createTextInstance, appendInitialChild }
