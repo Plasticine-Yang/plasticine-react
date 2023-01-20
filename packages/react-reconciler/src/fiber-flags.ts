308aee3b60e85d510a004c812e6566f1b1f0ee4b
plasticine9750 <975036719@qq.com>
2023-01-20 15:15:54 +0800

    feat(react-reconciler): fiber-flags -- add MutationMask

--- packages/react-reconciler/src/fiber-flags.ts
+++ packages/react-reconciler/src/fiber-flags.ts
@@ -7,6 +7,14 @@ export enum Flags {
   /** @description 插入元素 */
   Placement = /*                    */ 0b000000000000000000000000010,
 
+  /** @description 更新元素 */
+  Update = /*                       */ 0b000000000000000000000000100,
+
   /** @description 删除元素 */
   ChildDeletion = /*                */ 0b000000000000000000000010000,
 }
+
+/**
+ * @description commit 阶段的 beforeMutation 子阶段需要处理的 fiber flags
+ */
+export const MutationMask = Flags.Placement | Flags.Update | Flags.ChildDeletion
