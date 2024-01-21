3e467632d8db0025bef65e0979f775116bdcd86c
plasticine9750 <975036719@qq.com>
2024-01-21 16:10:34 +0800

    feat(stack-reconciler): export FunctionCopmonent type

--- packages/stack-reconciler/src/index.ts
+++ packages/stack-reconciler/src/index.ts
@@ -1,4 +1,4 @@
-export { ClassComponent, type ReactElement } from '@plasticine-react/shared'
+export { ClassComponent, type FunctionComponent, type ReactElement } from '@plasticine-react/shared'
 export type * from './types'
 
 export * from './mount'
